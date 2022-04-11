da577aa09420222b4c8d66ad9a0c5f250979ec4f
Minh <58528178+cuddddd@users.noreply.github.com>
2022-04-11 10:11:03 -0400

    Fixed Balance updating issues for Chequing and Savings

--- BankApp/src/my/bankapp/Savings.java
+++ BankApp/src/my/bankapp/Savings.java
@@ -9,23 +9,33 @@ package my.bankapp;
  * @author mdoan3
  */
 public class Savings {
-    protected double savBal = 0;
+    protected static double savBal = 0;
     private final double interest = 0.20;
     
     public void addInterest(){
         this.savBal = (this.savBal * interest) + this.savBal;
     }
     
+    public void setSavings(double amount){
+        this.savBal = amount;
+    }
+    
     public double checkBalance(){
-        return this.savBal;
+        return savBal;
     }
     
     public void addFunds(double amount){
-        System.out.println("Added Funds" + amount);
-        this.savBal += amount;
+        System.out.println("Added Funds to Savings: " + amount);
+        double newBal = checkBalance();
+        newBal += amount;
+        setSavings(newBal);
+        //System.out.println(newBal);
     }
     public void removeFunds(double amount){
-      return this.savBal -= amount;
+        System.out.println("Removed Funds from Savings: " + amount);
+        double newBal = checkBalance();
+        newBal = newBal - amount;
+        setSavings(newBal);
     }
         
 }
